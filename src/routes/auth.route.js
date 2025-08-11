f76dd92335c83533de4d05816efc2e0a1f6a909e
Neeraj-Pant01 <npant923@gmail.com>
2025-08-11 23:34:44 +0530

    updated

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,4 +1,11 @@
+const { register, login } = require("../controllers/auth.controller");
+
 const router = require("express").Router();
+
+router.get('/register',register)
+
+router.get('/login',login)
+
 
 
 module.exports = router;
